c786fa07273f0c8c3fd33f825f12698275d66d93
Rohith Shivakumar <rohitshivakumar1@gmail.com>
2024-03-13 01:04:44 +0530

    predicted output

--- multiple_liear.py
+++ multiple_liear.py
@@ -16,7 +16,18 @@ from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 x_transformed = ct.fit_transform(x)
 x_transformed[:, 3] = x_transformed[:, 3].round(1)
-x_transformed_df = pd.DataFrame(x_transformed)
-x = x_transformed_df
+x = pd.DataFrame(x_transformed)
 
 print(x)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size= 0.2, random_state= 0)
+
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.values.reshape(len(y_test), 1)),1))
